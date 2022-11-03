3f528e37e4dcae93850ffce48adf9c6f3a0b2545
alumno <alumno@A301PC14>
2022-11-03 12:48:28 +0100

    actualizo ejercicio tema 4 superheroes

--- Tema4-Superheroes/MainWindow.xaml.cs
+++ Tema4-Superheroes/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
-using Tema4_Superheroes.modelos;
+using Tema4_Superheroes.vista_modelo;
 
 namespace Tema4_Superheroes
 {
@@ -10,57 +9,22 @@ namespace Tema4_Superheroes
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<Superheroe> superheroes;
+        private MainWindowVM vm = new MainWindowVM();
 
         public MainWindow()
         {
             InitializeComponent();
-            superheroes = Superheroe.GetSamples();
-            contenedorDockPanel.DataContext = superheroes;
+            DataContext = vm;
         }
 
         private void AnteriorImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            int contador = int.Parse(anteriorImage.Tag.ToString());
-            if (contador >= 1 && contador <= 3)
-            {
-                if (contador == 1)
-                {
-                    numeroTextBlock.Text = "1/3";
-                    anteriorImage.Tag = 1;
-                    siguienteImage.Tag = 1;
-                }
-                else
-                {
-                    contador--;
-                    numeroTextBlock.Text = $"{contador}/3";
-                    anteriorImage.Tag = contador;
-                    siguienteImage.Tag = contador;
-                    contenedorDockPanel.DataContext = superheroes[contador - 1];
-                }
-            }
+            vm.Retroceder();
         }
 
         private void SiguienteImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            int contador = int.Parse(siguienteImage.Tag.ToString());
-            if (contador >= 1 && contador <= 3)
-            {
-                if (contador == 3)
-                {
-                    numeroTextBlock.Text = "3/3";
-                    siguienteImage.Tag = 3;
-                    anteriorImage.Tag = 3;
-                }
-                else
-                {
-                    contador++;
-                    numeroTextBlock.Text = $"{contador}/3";
-                    siguienteImage.Tag = contador;
-                    anteriorImage.Tag = contador;
-                    contenedorDockPanel.DataContext = superheroes[contador - 1];
-                }
-            }
+            vm.Avanzar();
         }
     }
 }
---
+++ Tema4-Superheroes/vista-modelo/MainWindowVM.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tema4_Superheroes.modelos;
+
+namespace Tema4_Superheroes.vista_modelo
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        private Superheroe heroeActual;
+
+        public Superheroe HeroeActual
+        {
+            get => heroeActual;
+            set
+            {
+                heroeActual = value;
+                NotifyPropertyChanged("HeroeActual");
+            }
+        }
+
+        private int numeroActual;
+
+        public int NumeroActual
+        {
+            get => numeroActual;
+            set
+            {
+                numeroActual = value;
+                NotifyPropertyChanged("NumeroActual");
+            }
+        }
+
+        private int totalHeroes;
+
+        public int TotalHeroes
+        {
+            get => totalHeroes;
+            set
+            {
+                totalHeroes = value;
+                NotifyPropertyChanged("TotalHeroes");
+            }
+        }
+
+        private readonly List<Superheroe> listaSuperheroes = Superheroe.GetSamples();
+
+        public MainWindowVM()
+        {
+            HeroeActual = listaSuperheroes[0];
+            NumeroActual = 1;
+            TotalHeroes = listaSuperheroes.Count;
+        }
+
+        public void Avanzar()
+        {
+            if (NumeroActual < TotalHeroes)
+            {
+                NumeroActual++;
+                HeroeActual = listaSuperheroes[NumeroActual - 1];
+            }
+        }
+
+        public void Retroceder()
+        {
+            if (NumeroActual > 1)
+            {
+                NumeroActual--;
+                HeroeActual = listaSuperheroes[NumeroActual - 1];
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
